5845764e19c895a7d3ceb20407f4b220694827d8
Izmaylova <Natalyatf@yandex.ru>
2022-11-21 23:52:13 +0500

    Числа Домашнее задание 1 Задание 3

--- src/Integer.java
+++ src/Integer.java
@@ -3,18 +3,5 @@ import java.util.Map;
 
 public class Integer {
 
-    private final Map<String, java.lang.Integer> map = new HashMap<>();
 
-    private void addToMap(String key, int value) {
-        if (!map.containsKey(key)) {
-            map.put(key, value);
-        } else {
-            int valueFromMap = map.get(key);
-            if (valueFromMap == value) {
-                throw new IllegalArgumentException("Внимание! Значения совпадают");
-            } else {
-                map.put(key, valueFromMap + value);
-            }
-        }
-    }
 }
