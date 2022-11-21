ed4c48006dc3cf15f24195a0fec00c6b9fb2c761
Izmaylova <Natalyatf@yandex.ru>
2022-11-21 23:37:17 +0500

    initial commit

--- src/Integer.java
+++ src/Integer.java
@@ -3,5 +3,18 @@ import java.util.Map;
 
 public class Integer {
 
+    private final Map<String, java.lang.Integer> map = new HashMap<>();
 
+    private void addToMap(String key, int value) {
+        if (!map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            int valueFromMap = map.get(key);
+            if (valueFromMap == value) {
+                throw new IllegalArgumentException("значения совпадают");
+            } else {
+                map.put(key, valueFromMap + value);
+            }
+        }
+    }
 }
