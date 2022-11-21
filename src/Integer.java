c44abec56ed719bfb48f373ccf07c828dfdbec3c
Izmaylova <Natalyatf@yandex.ru>
2022-11-21 23:52:52 +0500

    Числа Домашнее задание 1 Задание 3

--- src/Integer.java
+++ src/Integer.java
@@ -2,6 +2,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Integer {
+    private final Map<String, java.lang.Integer> map = new HashMap<>();
+
+    private void addToMap(String key, int value) {
+        if (!map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            int valueFromMap = map.get(key);
+            if (valueFromMap == value) {
+                throw new IllegalArgumentException("Значения совпадают");
+            } else {
+                map.put(key, valueFromMap + value);
+            }
+        }
+    }
 
 
 }
